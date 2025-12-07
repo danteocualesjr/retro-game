6789e5f4429f27b7ba78e37e74efd53d86c962fe
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2025-12-07 18:18:49 +0800

    ui tweak

--- main.js
+++ main.js
@@ -60,8 +60,9 @@ const bullets = [];
 const enemies = [];
 const particles = [];
 const stars = new Array(90).fill(0).map(() => ({
-  
+
   x: Math.random() * canvas.width,
+  
   y: Math.random() * canvas.height,
   speed: 40 + Math.random() * 90,
   size: 1 + Math.random() * 2,
