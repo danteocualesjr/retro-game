a6ca9bd9ac9291cc105953a4f855c85e1ed2fd39
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2025-12-09 01:53:42 +0800

    ui tweak

--- main.js
+++ main.js
@@ -101,10 +101,11 @@ function startGame() {
   hideOverlay();
   state.lastTime = performance.now();
   requestAnimationFrame(loop);
-  
+
 }
 
 function gameOver() {
+  
   state.running = false;
   state.gameOver = true;
   const isNewHighScore = state.score > state.highScore;
